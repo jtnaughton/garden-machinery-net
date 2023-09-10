0ea1aac4874f2d8d15dc4af561ca34c5b5cc3bca
James-naughton <64196213+James-naughton@users.noreply.github.com>
2023-09-10 16:43:16 +0100

    basic landing page plus nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import landingPage from './images/landingPage.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	return (
+		<div>
+			<nav class="navbar background">
+				<ul class="nav-list">
+				    <h4>Dave Naughton Mower Service</h4>
+					<li><a href="#courses">Servicing and Repairs</a></li>
+					<li><a href="#tutorials">Spare Parts</a></li>
+					<li><a href="#jobs">Refurbished Lawn Mowers</a></li>
+				</ul>
+
+				<div class="rightNav">
+					<input type="text" name="search" id="search" />
+					<button class="btn btn-sm">Search</button>
+				</div>
+			</nav>
+
+	<div class="box-main">
+	<div style={{ backgroundImage: `url(${landingPage})` }}>
+      Hello World
     </div>
-  );
+		</div>			
+			<footer className="footer">
+				<p className="text-footer">
+					Copyright ©-All rights are reserved
+				</p>
+			</footer>
+		</div>
+	)
 }
 
-export default App;
+export default App
