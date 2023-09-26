5ff500f72865df158038255253c3d64130517fdf
James-naughton <64196213+James-naughton@users.noreply.github.com>
2023-09-26 09:56:41 +0100

    css updates

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,29 +8,31 @@ import { NavBar } from "../navigation/NavBarComponent";
 export const Home = () => {
   return (
     <>
-      <div
-        className="image"
-        style={{ backgroundImage: `url(${grassLawnWithConcreteWall})` }}
-      >
-        <NavBar />
-        <div className="container">
-          <div className="landingPage d-flex flex-row justify-content-start align-items-start">
-            <h1 className="landingPageTitle">David Naughton Mowers.</h1>
+      <div className="container">
+        <div
+          className="image"
+          style={{ backgroundImage: `url(${grassLawnWithConcreteWall})` }}
+        >
+          <NavBar />
+          <div className="landingPage d-flex flex-row justify-content-between align-items-start">
+            <h1 className="landingPageTitle align-self-start">
+              David Naughton Garden Machinery.
+            </h1>
             <p className="landingPageDes align-self-center">
               High quality refurbished Lawnmowers and spare parts. <br />
               Unbeatable prices.
-            </p>
-          </div>
-          <div className="buttonContainer align-self-end">
+              <div className="buttonContainer align-self-center pt-3">
               <Link className="shopNowButton" to="/shop">
                 Shop
               </Link>
+            </div>
+            </p>
           </div>
         </div>
+        <footer className="footer">
+          <p className="text-footer">Copyright ©-All rights are reserved</p>
+        </footer>
       </div>
-      <footer className="footer">
-        <p className="text-footer">Copyright ©-All rights are reserved</p>
-      </footer>
     </>
   );
 };
